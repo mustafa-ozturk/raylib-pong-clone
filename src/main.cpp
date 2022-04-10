bd94e1ecf0944498228b4e8d632437f7dbba3558
Mustafa Ozturk <mozturkpro@gmail.com>
2022-04-10 09:50:23 -0400

    add gamescreen state

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include "raylib.h"
 
+typedef enum GameScreen
+{
+    START,
+    GAME,
+    END
+} GameScreen;
+
 int main()
 {
     // init
     const int screenWidth = 800;
     const int screenHeight = 600;
+    GameScreen currentScreen = GameScreen::START;
+
     InitWindow(screenWidth, screenHeight, "raylib pong clone");
     SetTargetFPS(60);
 
@@ -13,7 +22,38 @@ int main()
     {
         BeginDrawing();
             // update
+            switch(currentScreen)
+            {
+                case GameScreen::START:
+                {
+
+                } break;
+                case GameScreen::GAME:
+                {
+
+                } break;
+                case GameScreen::END:
+                {
+
+                } break;
+            } 
             // draw
+            ClearBackground(BLACK);
+            switch(currentScreen)
+            {
+                case GameScreen::START:
+                {
+                    
+                } break;
+                case GameScreen::GAME:
+                {
+
+                } break;
+                case GameScreen::END:
+                {
+
+                } break;
+            } 
         EndDrawing();
     }
 
